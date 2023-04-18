7f95c360c0e95abed75423bd778162feb17e88c7
Michael Parekh <mparekh4@illinois.edu>
2023-04-18 11:41:02 -0400

    init proj

---
+++ AVKit Player/AVKit Player/AVKit_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVKit_PlayerApp.swift
+//  AVKit Player
+//
+//  Created by Michael Parekh on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AVKit_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
