e3b7b3f5cd2ef21f95d7a7e92a9ffabe5323492f
Michael Parekh <mparekh4@illinois.edu>
2023-04-19 20:35:10 -0400

    impl the song model

---
+++ AVKit Player/AVKit Player/MeditationViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MeditationViewModel.swift
+//  AVKit Player
+//
+//  Created by Michael Parekh on 4/19/23.
+//
+
+import Foundation
+
+// A model is used to pass data around the application efficiently (not necessary for this example app where we only have one song in the colleciton).
+struct Meditation {
+    let id = UUID()
+    let title: String
+    let description: String
+    let duration: TimeInterval
+    let track: String
+    let image: String
+    
+    // Instantiate the 'Meditation' dummy data once and store it as a static variable ('static' allows the property to be shared among all instances of the 'struct').
+    static let data = Meditation(title: "1 Minute Relaxing Meditation", description: "Clear your mind and slumber into nothingness. Allocate only a few moments for a quick breather.", duration: 70, track: "meditation1", image: "image-feather")
+}
